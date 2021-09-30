10d11959ca849010a268c2775696d9660578425f
JoseFigueroa21 <75599007+JoseFigueroa21@users.noreply.github.com>
2021-09-30 13:21:28 -0700

    Changes done on my.cpp
    name

--- jfigueroa.cpp
+++ jfigueroa.cpp
@@ -8,5 +8,5 @@ void show_jose_credits(int x, int y)
     r.bot = y;
     r.left = x;
     r.center = 1;
-    ggprint8b(&r, 16, 0x00ff0000, "--Jose Figueroa: credits-- ");
+    ggprint8b(&r, 16, 0x00ff0000, "Jose Figueroa");
 }
